1802f3f337717ab8e0f9b04b435a8c446de35648
Satyen Akolkar <s.akolkar3@gmail.com>
2017-01-28 14:40:41 -0700

    added list view item

--- app/src/main/java/com/andreiusenka/hackerlist/Tasks/TaskFragment.java
+++ app/src/main/java/com/andreiusenka/hackerlist/Tasks/TaskFragment.java
@@ -6,14 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import com.andreiusenka.hackerlist.R;
+import com.andreiusenka.hackerlist.data.Task;
+
+import java.util.ArrayList;
 
 
 public class TaskFragment extends Fragment implements TaskContract.View  {
 
     private TaskContract.Presenter mTaskPresenter;
+
+    private View noTasksView;
+    private LinearLayout tasksView;
+    private TaskAdapter listAdapter;
 
     private ListView taskList;
 
@@ -38,5 +48,37 @@ public class TaskFragment extends Fragment implements TaskContract.View  {
         // Setup the task list
         taskList = (ListView) root.findViewById(R.id.task_activity_listview);
         return root;
+    }
+
+    private static class TaskAdapter extends BaseAdapter {
+        private ArrayList<Task> tasks;
+
+        @Override
+        public int getCount() {
+            return tasks.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return tasks.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            View listView = view;
+            if (listView == null) {
+                LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+                listView = inflater.inflate(R.layout.task_listitem, viewGroup, false);
+            }
+
+            return listView;
+
+
+        }
     }
 }
---
+++ app/src/main/java/com/andreiusenka/hackerlist/data/Task.java
@@ -0,0 +1,12 @@
+package com.andreiusenka.hackerlist.data;
+
+/**
+ * Created by satyen on 28/01/17.
+ */
+
+public class Task {
+
+
+
+
+}
