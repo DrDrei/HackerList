3a6291e715a335e5f8590d4891ad1ec81ea07f08
Andrei <drei3000@gmail.com>
2017-01-28 13:51:07 -0700

    added task xml and boiler plate code for the listview

--- app/src/main/java/com/andreiusenka/hackerlist/Tasks/TaskFragment.java
+++ app/src/main/java/com/andreiusenka/hackerlist/Tasks/TaskFragment.java
@@ -1,11 +1,21 @@
 package com.andreiusenka.hackerlist.Tasks;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.andreiusenka.hackerlist.R;
 
 
 public class TaskFragment extends Fragment implements TaskContract.View  {
 
     private TaskContract.Presenter mTaskPresenter;
+
+    private ListView taskList;
 
     public static TaskFragment newInstance() {
         return new TaskFragment();
@@ -14,5 +24,19 @@ public class TaskFragment extends Fragment implements TaskContract.View  {
     @Override
     public void setPresenter(TaskContract.Presenter presenter) {
         mTaskPresenter = presenter;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.task_fragment, container, false);
+
+        // Setup the task list
+        taskList = (ListView) root.findViewById(R.id.task_activity_listview);
+        return root;
     }
 }
